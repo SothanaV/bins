26d1a5b62f28a4ed4362bb635b753b20bd237998
SothanaV <nai6320@gmail.com>
2019-10-29 16:40:28 +0700

    add command

---
+++ run_first.sh
@@ -0,0 +1,8 @@
+virtualenv env
+source env/bin/activate
+cd video_detection
+pip install -r requirements.txt
+# git clone https://gitlab.com/SothanaV/darknet.git
+# cd darknet
+# wget https://pjreddie.com/media/files/yolov3.weights
+# make
