90a0b9509ea8ae9bf327f81e958b061dcca49431
SothanaV <nai6320@gmail.com>
2020-02-07 19:09:16 +0700

    update ino

--- mcu/02/02.ino
+++ mcu/02/02.ino
@@ -5,7 +5,7 @@ void setup()
 {
     Serial.begin(115200); 
     servo.attach(15); // pin
-    servo.write(0);
+    servo.write(180);
     for(uint8_t t = 3; t > 0; t--) 
     {
         Serial.printf("[SETUP] WAIT %d...\n", t);
@@ -16,13 +16,13 @@ void setup()
 
 void loop()
 {
-    if (Serial.available() > 0) 
-    {
+    // if (Serial.available() > 0) 
+    // {
         String payload = Serial.readString();
         Serial.printf("%s \n","Payload");
         Serial.println(payload);
         angle = payload.toInt();
         servo.write(angle);
         delay(100);
-    }
+    // }
 }
